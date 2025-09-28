d88c1f919be3b723980f6497f800055e771618f8
ElCamacho05 <camachocastelanjm@gmail.com>
2025-09-27 21:05:03 -0600

    clase notas completada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.core.ktx)
+    implementation(libs.room.common.jvm)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/db_classes/Note.java
+++ app/src/main/java/db_classes/Note.java
@@ -1,5 +1,27 @@
 package db_classes;
 
-public class Note {
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+@Entity(tableName = "note")
+public class Note implements Comparable<Note>{
+
+    @PrimaryKey (autoGenerate = true)
+    public Integer id;
+    public String title;
+    public String content;
+    public String date;
+
+    public String lastModified;
+
+    public Note(String title, String content, String date) {
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.lastModified = date;
+    }
+
+    public int compareTo(Note note){
+        return Integer.compare(this.id, note.id);
+    }
 }
